87634a773c50331f6b19e7582a14a7220939649a
Faboid <72757782+Faboid@users.noreply.github.com>
2023-07-04 15:12:06 +0200

    Commented out the skills section in App()

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import About from './sections/About';
 import Contact from './sections/Contact';
 import Projects from './sections/Projects';
-import Skills from './sections/Skills';
+//import Skills from './sections/Skills';
 
 function App() {
   return (
     <div className="App">
       <About/>
-      <Skills/>
+      {/* <Skills/> */}
       <Projects/>
       <Contact/>
     </div>
