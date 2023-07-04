e7968fd2a7e8780a6fbdcebd2c126f6551ba2ed5
Faboid <72757782+Faboid@users.noreply.github.com>
2023-07-04 21:51:04 +0200

    Set up basic layout for the single projects.

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -1,8 +1,52 @@
+import projects from '../data/projects.json';
+import './Projects.css';
 
 export default function Projects() {
+
     return (
         <div className="projects">
             <h1>My Projects</h1>
+            {projects.map((item, index) => {
+                return <Project key={index} project={item} index={index}/>;
+            })}
         </div>
     );
 };
+
+function Project({ project, index }) {
+
+    let order = (index & 1) === 0;
+    let classN = "project " + (order ? "" : "mirrored");
+
+    return (
+        <div className='project-container'>
+            <div className={classN}>
+
+                <img className="project-image" src={process.env.PUBLIC_URL + '/images/projects' + project.image} alt="view"/>
+
+                <div className="project-text">
+                    <div className='project-header'>
+                        <span className='title'>{project.title}</span>
+                        <span><a href={project.github}>GH</a></span>
+                    </div>
+                    <p>{project.description}</p>
+
+                    <div className='project-techs'>
+                        {project.tech.map((element) => {
+                            return <Tech key={element.title} tech={element}/>;
+                        })}
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+function Tech({ tech }) {
+    return (
+        <div className='tech'>
+            <span className='tech-text'>{tech}</span>
+        </div>
+    );
+}
