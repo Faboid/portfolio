0945a60906cd14ef08d59bc32e1c3bbd6980e9c6
Faboid <72757782+Faboid@users.noreply.github.com>
2023-07-06 11:07:19 +0200

    Ensured that github links are opened in another tab.

--- src/components/GithubMark.js
+++ src/components/GithubMark.js
@@ -3,7 +3,7 @@ import './GithubMark.css';
 export default function GithubMark({ link }) {
     return (
         <div className="github-mark">
-            <a className='github-link' href={link}> 
+            <a className='github-link' href={link} target='_blank' rel="noreferrer"> 
             </a>
         </div>
     );
