c7a1074e2f3d67448486e5bd32ba95a9e951a038
Faboid <72757782+Faboid@users.noreply.github.com>
2023-07-07 17:57:18 +0200

    Set up contact form layout.

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -1,8 +1,44 @@
+import './Contact.css';
+import Spacer from '../components/Spacer';
 
 export default function Contact() {
     return (
         <div className="contact">
+            <Spacer height="5vh"/>
             <h1>Contact me</h1>
+            <ContactForm/>
         </div>
     );
 };
+
+function ContactForm() {
+    return (
+        <div className='contact-form'>
+            <div className='contact-form-inner'>
+                <Spacer height={"2rem"}/>
+
+                <div className='form-input-container'>
+                    <label className='form-input-label'>Name</label>
+                    <input name='name' className='form-input'/>
+                </div>
+
+                <div className='form-input-container'>
+                    <label className='form-input-label'>Email</label>
+                    <input name='email' className='form-input'/>
+                </div>
+
+                <div className='form-input-container'>
+                    <label className='form-input-label'>Subject</label>
+                    <input name='subject' className='form-input'/>
+                </div>
+
+                <div className='form-input-container'>
+                    <label className='form-input-label'>Message</label>
+                    <textarea name='message' className='form-input'/>
+                </div>
+
+                <button className='form-submit-btn'>Send</button>
+            </div>
+        </div>
+    );
+}
