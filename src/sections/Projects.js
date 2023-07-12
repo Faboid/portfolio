4479906c116acf947eb2bfae87d3293acb9e8620
Faboid <72757782+Faboid@users.noreply.github.com>
2023-07-12 20:41:54 +0200

    Created new layout to experiment with other ways of showcasing projects.

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -6,55 +6,46 @@ export default function Projects() {
 
     return (
         <div className="projects">
+
             <h1>My Projects</h1>
 
             <div className='projects-wrapper'>
-                {projects.map((item, index) => {
-                    return <Project key={item.title} project={item} index={index}/>;
+                {projects.map(item => {
+                    return <Project key={item.title} project={item}/>;
                 })}
             </div>
         </div>
     );
 };
 
-function Project({ project, index }) {
-
-    let order = (index & 1) === 0;
-    let classN = "project " + (order ? "" : "mirrored");
+function Project({ project}) {
 
     return (
-        <div className='project-container'>
-            <div className={classN}>
+        <div className='project'>
 
-                <img className="project-image" src={process.env.PUBLIC_URL + '/images/projects' + project.image} alt="view"/>
+            <div className='project-text-area'>
 
-                <div className="project-text">
-                    <div className='project-header'>
-                        <span className='project-title'>{project.title}</span>
-                        <GithubMark link={project.github}/>
-                    </div>
-
-                    <p className='project-description'>{project.description}</p>
-
-                    <div>
-                        <span className='project-tech-header'>Built with:</span>
-                        <div className='project-techs'>
-                            {project.tech.map((element, index) => {
-                                return <Tech key={index} tech={element}/>;
-                            })}
-                        </div>
-                    </div>
+                <div className='project-header'>
+                    <span className='project-title'>{project.title}</span>
+                    <GithubMark link={project.github}/>
                 </div>
 
+                <p className='project-description'>{project.description}</p>
+                
             </div>
-        </div>
-    );
-}
 
-function Tech({ tech }) {
-    return (
-        <div className='tech'>
-            <span className='tech-text'>{tech}</span>
+            <div className='project-image-area'>
+                <img className="project-image" src={process.env.PUBLIC_URL + '/images/projects' + project.image} alt="view"/>
+            </div>
+
+            <div className='project-tech-area'>
+                {project.tech.map((element) => {
+                    return (
+                        <span key={element} className='tech'>{element}</span>
+                    );
+                })}
+            </div>
+
         </div>
     );
 }
