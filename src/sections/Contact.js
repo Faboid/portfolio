0bdf5b82bc3378062e790f573ff2376d3fd92251
Faboid <72757782+Faboid@users.noreply.github.com>
2023-07-13 20:34:07 +0200

    Improved responsiveness to different screen sizes for the contact section.

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -5,7 +5,7 @@ import config from '../config.json';
 export default function Contact() {
     return (
         <div className="contact">
-            <Spacer height="5vh"/>
+            <Spacer height="3vh"/>
             <h1>Contact me</h1>
             <ContactForm/>
         </div>
@@ -27,24 +27,23 @@ function ContactForm() {
                     <input type='text' name='_hon' className='hon-pot'/>
                     <input type="hidden" name="_captcha" value="false"/>
 
-                    <div className='form-input-container'>
-                        <label className='form-input-label'>Name</label>
-                        <input name='name' className='form-input'/>
-                    </div>
+                    <div className='form-inputs'>
 
-                    <div className='form-input-container'>
-                        <label className='form-input-label'>Email</label>
-                        <input name='email' className='form-input'/>
-                    </div>
+                        <div className='form-input-container'>
+                            <label className='form-input-label'>Name</label>
+                            <input name='name' className='form-input'/>
+                        </div>
 
-                    {/*<div className='form-input-container'>
-                        <label className='form-input-label'>Subject</label>
-                        <input name='subject' className='form-input'/>
-                    </div> */}
+                        <div className='form-input-container'>
+                            <label className='form-input-label'>Email</label>
+                            <input name='email' className='form-input'/>
+                        </div>
 
-                    <div className='form-input-container'>
-                        <label className='form-input-label'>Message</label>
-                        <textarea name='message' className='form-input'/>
+                        <div className='form-input-container'>
+                            <label className='form-input-label'>Message</label>
+                            <textarea name='message' className='form-input'/>
+                        </div>
+
                     </div>
 
                     <button className='form-submit-btn'>Send</button>
