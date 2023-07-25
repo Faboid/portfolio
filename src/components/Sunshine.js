c95b0c6ebbb102c1e1cadc15ddd819ac1a54c355
Faboid <72757782+Faboid@users.noreply.github.com>
2023-07-25 19:02:05 +0200

    Reduced max stars amount for performance reasons and improved responsiveness by applying left: in percentages instead of a fixed pixel value.

--- src/components/Sunshine.js
+++ src/components/Sunshine.js
@@ -20,7 +20,7 @@ function StarsContainer() {
 
     let starsAmount = Math.floor((window.innerHeight * window.innerWidth) / 20000);
     starsAmount = Math.max(50, starsAmount);
-    starsAmount = Math.min(500, starsAmount);
+    starsAmount = Math.min(350, starsAmount);
 
     return (
         <div className="stars-container">
@@ -35,13 +35,13 @@ function StarsContainer() {
 function Star({ maxSize, sunPercentageXPos }) {
 
     const randomX = Math.random();
-    const positionX = Math.floor(randomX * window.innerWidth);
-    const positionY = Math.floor(Math.random() * window.innerHeight);
+    const positionX = Math.floor(randomX * 100);
+    const positionY = Math.floor(Math.random() * 100);
     const height = Math.random() * maxSize;
     const opacity = Math.abs(randomX - sunPercentageXPos) % 1;
 
     return (
-        <div className="star" style={{ left: `${positionX}px`, top: `${positionY}px`, height: `${height}rem`, opacity: opacity}}>
+        <div className="star" style={{ left: `${positionX}%`, top: `${positionY}%`, height: `${height}rem`, opacity: opacity}}>
 
         </div>
     ); 
