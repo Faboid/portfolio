c6f6101df49781167041a39dd543b9e5b90a6ebf
Faboid <72757782+Faboid@users.noreply.github.com>
2023-08-14 22:56:18 +0200

    Set up dynamic label.

--- src/components/FormInputField.js
+++ src/components/FormInputField.js
@@ -1,13 +1,25 @@
+import { useState } from 'react';
 import './FormInputField.css';
 
 export default function FormInputField({ name, placeholder, multiline }) {
 
+    const [hasValue, setHasValue] = useState(false);
+    const labelClass = "input-label " + ((hasValue) ? "show" : "");
+
+    function CheckIfEmpty(e) {
+        let status = e.target.value.length > 0;
+        if(status != hasValue) {
+            setHasValue(() => status);
+        }
+    }
+
     const inputField = (multiline) ?
-        <textarea name={name} placeholder={placeholder} className='input-field'/> :
-        <input name={name} placeholder={placeholder} className='input-field'/>;
+        <textarea name={name} placeholder={placeholder} onChange={(e) => CheckIfEmpty(e)} className='input-field'/> :
+        <input name={name} placeholder={placeholder} onChange={(e) => CheckIfEmpty(e)} className='input-field'/>;
 
     return (
         <div className='input-container'>
+            <label className={labelClass} htmlFor={name}>{placeholder}</label>
             {inputField}
         </div>
     );
