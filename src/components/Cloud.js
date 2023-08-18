ac5a58e07f20c6653ea6c60a2ceb20b7edd2ec62
Faboid <72757782+Faboid@users.noreply.github.com>
2023-08-18 23:55:03 +0200

    Began developing the submission form background—Cloud.js

---
+++ src/components/Cloud.js
@@ -0,0 +1,40 @@
+import "./Cloud.css";
+
+export default function Cloud() {
+
+    const spheres = [
+        { left: 2, top: 35 },
+        { left: 18, top: 2 },
+        { left: 40, top: 7 },
+        { left: 60, top: 1 },
+        { left: 85, top: 8 },
+        { left: 99, top: 40 },
+
+        { left: 30, top: 50 },
+        { left: 65, top: 45 },
+
+        { left: 10, top: 80 },
+        { left: 25, top: 95 },
+        { left: 45, top: 90 },
+        { left: 70, top: 92 },
+        { left: 90, top: 86 },
+    ];
+
+    return (
+        <div className="cloud">
+            {spheres.map((item, index) => {
+                return <CloudSphere key={index} left={item.left} top={item.top}/>
+            })}
+        </div>
+    );
+}
+
+function CloudSphere({ left, top }) {
+
+    const style = {
+        "--startX" : left + '%',
+        "--startY" : top + '%'
+    }
+    
+    return <div className="cloud-sphere" style={style}></div>
+}
