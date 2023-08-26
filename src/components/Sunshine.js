9b0b6f1c9b1c09c31f48105d2a5d7a1ce3fdcc44
Faboid <72757782+Faboid@users.noreply.github.com>
2023-08-26 11:56:08 +0200

    Implemented sunshine-obscurer, to darken the about section when scrolling down.

--- src/components/Sunshine.js
+++ src/components/Sunshine.js
@@ -1,43 +1,61 @@
-import './Sunshine.css';
+import { useEffect, useRef, useState } from "react";
+import "./Sunshine.css";
 
 export default function Sunshine({ children }) {
-
     return (
         <div className="sunshine-container">
-            <div className='sunshine'>
-                <StarsContainer/>
-                <Sun/>
+            <div className="sunshine">
+                <StarsContainer />
+                <Sun />
             </div>
 
             {children}
+
+            <Obscurer />
         </div>
     );
 }
 
+function Obscurer() {
+    const [obscurerAlpha, setObscurerAlpha] = useState(0);
+
+    useEffect(() => {
+        document.addEventListener("scroll", handleScroll);
+
+        return () => {
+            document.removeEventListener("scroll", handleScroll);
+        };
+
+        function handleScroll(e) {
+            const alpha = Math.min(0.8, window.scrollY / window.innerHeight).toFixed(2);
+            setObscurerAlpha(alpha);
+        }
+    });
+
+    return <div style={{ "--alpha": obscurerAlpha }} className="sunshine-obscurer"></div>;
+}
+
 function Sun() {
-    return (
-        <div className="sun"></div>
-    );
+    return <div className="sun"></div>;
 }
 
 function StarsContainer() {
-
     let starsAmount = Math.floor((window.innerHeight * window.innerWidth) / 20000);
     starsAmount = Math.max(50, starsAmount);
     starsAmount = Math.min(350, starsAmount);
 
     return (
         <div className="stars-container">
-            {Array(starsAmount).fill(0).map((_, index) => {
-                return <Star key={index} maxSize={0.5} sunPercentageXPos={0.8}/>;
-            })}
+            {Array(starsAmount)
+                .fill(0)
+                .map((_, index) => {
+                    return <Star key={index} maxSize={0.5} sunPercentageXPos={0.8} />;
+                })}
         </div>
     );
-
 }
 
 function Star({ maxSize, sunPercentageXPos }) {
-
     const randomX = Math.random();
     const positionX = Math.floor(randomX * 100);
     const positionY = Math.floor(Math.random() * 100);
@@ -45,9 +63,13 @@ function Star({ maxSize, sunPercentageXPos }) {
     const opacity = Math.abs(randomX - sunPercentageXPos) % 1;
 
     return (
-        <div className="star" style={{ left: `${positionX}%`, top: `${positionY}%`, height: `${height}rem`, opacity: opacity}}>
-
-        </div>
-    ); 
-
+        <div
+            className="star"
+            style={{
+                left: `${positionX}%`,
+                top: `${positionY}%`,
+                height: `${height}rem`,
+                opacity: opacity
+            }}></div>
+    );
 }
