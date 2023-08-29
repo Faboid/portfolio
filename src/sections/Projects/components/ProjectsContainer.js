a1e71070d42fb0cd4504f0d17648b48580bc41a7
Faboid <72757782+Faboid@users.noreply.github.com>
2023-08-29 10:10:59 +0200

    Extracted ProjectBG to its own file.

--- src/sections/Projects/components/ProjectsContainer.js
+++ src/sections/Projects/components/ProjectsContainer.js
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
+import ProjectBG from './ProjectBG';
 import GithubMark from '../components/GithubMark';
 import ZoomableImage from '../components/ZoomableImage';
 import './ProjectsContainer.css';
@@ -113,41 +114,6 @@ function Project({ project, getParentRect, clientX, clientY }) {
                 })}
             </div>
 
-        </div>
-    );
-}
-
-function ProjectBG({ getParentRect, clientX, clientY }) {
-    
-    const bgRef = useRef(null);
-    const [lightX, setLightX] = useState(0);
-    const [lightY, setLightY] = useState(0);
-
-    //handle light
-    useEffect(() => {
-        const target = bgRef.current;
-        const rect = target.getBoundingClientRect();
-        const parent = getParentRect();
-
-        //calc
-        const x = clientX - parent.left;
-        const y = clientY - parent.top;
-        const xDiff = rect.left - parent.left;
-        const yDiff = rect.top - parent.top;
-        const newLightX = x - xDiff + 'px';
-        const newLightY = y - yDiff + 'px';
-
-        setLightX(newLightX);
-        setLightY(newLightY);
-
-    }, [getParentRect, clientX, clientY]);
-
-    return (
-        <div ref={bgRef} className='project-bg'>
-            <div style={{ 
-                "--mouse-x": lightX,
-                "--mouse-y": lightY
-             }} className='project-light'></div>
         </div>
     );
 }
