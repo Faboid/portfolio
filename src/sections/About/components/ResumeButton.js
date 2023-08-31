cfeb93e9bd4e869119be2f0376ad7e9f3458706b
Faboid <72757782+Faboid@users.noreply.github.com>
2023-08-31 18:42:21 +0200

    Removed the wrapping div and used button as the only element in ResumeButton.

--- src/sections/About/components/ResumeButton.js
+++ src/sections/About/components/ResumeButton.js
@@ -5,14 +5,17 @@ export default function ResumeButton() {
 
     const [clicked, setClicked] = useState(false);
     let btnClassName = 'resume-button ' + (clicked ? 'clicked' : '');
-
+    
+    function onClick(e) {
+        setClicked(true);
+    }
+    
     return (
-        <div className='resume-button-container'
-            onMouseDown={() => setClicked(true)}
-            onAnimationEnd={() => setClicked(false)}
-            >
-            <button className={btnClassName} disabled={clicked}>Resume</button>
-        </div>
+        <button 
+            className={btnClassName} 
+            disabled={clicked}
+            onClick={e => onClick(e)}
+            onAnimationEnd={() => setClicked(false)}>Resume</button>
     );
 }
 
