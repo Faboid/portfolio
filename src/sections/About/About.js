4b423a6e5bcd020a8348c1ffdc0325688037da21
Faboid <72757782+Faboid@users.noreply.github.com>
2023-09-07 22:38:49 +0200

    Set up about text.

--- src/sections/About/About.js
+++ src/sections/About/About.js
@@ -21,7 +21,7 @@ function Greetings() {
     return (
         <div className='greetings'>
             <div className='greetings-container'>
-                <h1 className='greeting-text'>Hello, I'm Name</h1>
+                <h1 className='greeting-text'>Hello, I'm Fabio.</h1>
                 <ResumeButton/>
             </div>
         </div>
@@ -34,9 +34,13 @@ function Description() {
             <div className='description-container'>
                 <h1 className='about-me-title'>About me</h1>
                 <div className='about-me-container'>
+
                     <p className='about-me-text'>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?
+                        In early 2020, a video on boids introduced me to the beauty of programming. Since then, I've learned how to code games, desktop and web applications.
+                    </p>
+
+                    <p className='about-me-text'>
+                        My journey has primarily been in the C#/.NET ecosystem, where I've cultivated a robust foundation. Using that foundation, I've lately branched out into web-focused technologies, with learning React as my latest endeavor.
                     </p>
                 </div>
             </div>
